4e7920999507d4235f75eb90aeb9cd83a9763dac
Priyavrat Singh Chouhan <priyavrat0072@gmail.com>
2023-08-02 13:03:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <h1>PROJECT 1</h1>
+    </div>
+  );
+}
+
+export default App;
