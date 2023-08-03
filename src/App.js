0f8a691954d37521154dfa077548b0bdcd692c98
Priyavrat Singh Chouhan <priyavrat0072@gmail.com>
2023-08-03 11:51:37 +0530

    id

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 
 import "./styles/App.scss"
 import "./styles/header.scss"
+import "./styles/home.scss"
+
+
 
 
 function App() {
@@ -12,8 +17,9 @@ function App() {
       <Router>
         <Header/>
         <Routes>
-          <Route />
+          <Route path='/' element={<Home/>} />
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import vg from "../assets/2.webp"
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai"
+
+function Home ()  {
+  return (
+       <>
+        <div className='home' id='home'>
+            <main>
+                <h1>TechyStar</h1>
+                <p>solution to all your problems</p>
+            </main>
+        </div>
+
+        <div className="home2">
+            <img src={vg} alt='Graphics'  />
+            <div>
+                <p>
+                    we are your ane and only solution to the tech problems you face
+                    everyday. we are leading teach company whose aim is to increase the problem solving ability in children.
+                </p>
+            </div>
+
+        </div>
+
+        <div className="home3" id='about'>
+            <div>
+                <h1>
+                    Who we are?
+                </h1>
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                </p>
+            </div>
+        </div>
+
+        <div className="home4" id='brands'>
+            <div>
+                <h1>
+                    Brands
+                </h1>
+                <article>
+                  
+                    <div style={{
+                        animationDelay: "0.3s"
+                    }}>
+                        <AiFillGoogleCircle/>
+                        <p>Google</p>
+                    </div>
+                    <div style={{
+                        animationDelay: "0.5s"
+                    }}>
+                        <AiFillAmazonCircle/>
+                        <p>Amazon</p>
+                    </div>
+                    <div style={{
+                        animationDelay: "0.7s"
+                    }}>
+                        <AiFillYoutube/>
+                        <p>Youtube</p>
+                    </div>
+                    <div style={{
+                        animationDelay: "0.9s"
+                    }}>
+                        <AiFillInstagram/>
+                        <p>Instagram</p>
+                    </div>
+
+                </article>
+            </div>
+        </div>
+
+       </>
+  )
+}
+
+export default Home
