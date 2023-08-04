08718ad1796998947962a365fd4e9f7ddb55f153
Priyavrat Singh Chouhan <priyavrat0072@gmail.com>
2023-08-04 12:34:20 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Footer  () {
   return (
-    <div>Footer</div>
+    <footer>
+    <div>
+        <h1>
+            Techystar
+        </h1>
+        <p>@all right reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow us</h5>
+        <div>
+        <a href="https://www.youtube.com/" target={"_blank"}>Youtube</a>
+        <a href="https://www.youtube.com/" target={"_blank"}>Instagram</a>
+        <a href="https://www.youtube.com/" target={"_blank"}>GitHub</a>
+        </div>
+    </div>
+    </footer>
+
   )
 }
 
